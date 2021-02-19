9b41f34864ccd4bd38d11392e759511df0863333
hyhong-code <hong961127@gmail.com>
2021-02-18 19:52:19 -0800

    Add storybook

--- sm-resolver.js
+++ sm-resolver.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import { Fragment } from 'react'
+import * as EssentialSlices from 'essential-slices/src/slices'
 
-export default ({ i }) => {
-  return () => i ? <div /> : <p>Your SliceZone is loading... You might need to reload this page.</p>
+const __allSlices = {  ...EssentialSlices, }
+
+const NotFound = ({ sliceName, slice, i }) => {
+  console.error(`[sm-resolver] component "${sliceName}" not found at index ${i}.`)
+  console.warn(`slice data: ${slice}`)
+	return process.env.NODE_ENV !== 'production' ? (
+    <div
+      style={{
+        height: '30vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        textAlign: 'center',
+        background: '#FAFAFA'
+      }}
+    >
+      <h2>
+        Slice "{sliceName}" not found.
+      </h2>
+      <p style={{ maxWidth: '320px', fontSize: '16px' }}>
+        Check that you registered this component in your slices library!
+      </p>
+    </div>
+  ) : <Fragment />
 }
+
+export default function SliceResolver({ sliceName, ...rest }) {
+	return __allSlices[sliceName] ? __allSlices[sliceName] : () => <NotFound sliceName={sliceName} {...rest} />
+}
+
+  
